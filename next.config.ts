0086f93dede3480a2b5d669fe8b9790cfc48df0e
MaverickDanielleAndres <maverickdanielle@gmail.com>
2025-12-07 19:13:32 +0800

    fix bugs

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-    eslint: {
-    ignoreDuringBuilds: true,
-  },
+const nextConfig = {
+  reactStrictMode: true,
 };
 
 export default nextConfig;
-
-
-// tailwind.config.js
-// module.exports = {
-//   theme: {
-//     extend: {
-//       keyframes: {
-//         shine: {
-//           '0%': { 'background-position': '100%' },
-//           '100%': { 'background-position': '-100%' },
-//         },
-//       },
-//       animation: {
-//         shine: 'shine 5s linear infinite',
-//       },
-//     },
-//   },
-//   plugins: [],
-// };
